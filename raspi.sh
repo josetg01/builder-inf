81bdfa0399c972488868c21914f82665ce6712b0
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-04-29 17:32:37 +0200

    Update raspi.sh

--- raspi.sh
+++ raspi.sh
@@ -8,6 +8,7 @@ sudo rc-update add docker
 sudo rc-service docker start
 sleep 30
 docker volume create portainer_data
+docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.9.3
 
 # Torrent client
 sudo rc-update add transmission-daemon
