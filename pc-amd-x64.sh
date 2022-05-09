a93eea8f50978a7e30ff6b4e6a27832ece4017b2
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-09 18:58:55 +0200

    Update pc-amd-x64.sh

--- pc-amd-x64.sh
+++ pc-amd-x64.sh
@@ -7,7 +7,6 @@ sudo apt install apt-transport-https curl
 sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
 sudo wget https://dl.google.com/linux/linux_signing_key.pub
 sudo apt-key add linux_signing_key.pub
-sudo apt update && sudo apt install -y google-chrome-stable
 
 #Instalar PhotoGIMP
 sudo flatpak install flathub org.gimp.GIMP -y
@@ -16,6 +15,6 @@ sudo flatpak install flathub org.gimp.GIMP -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
-sudo apt install -y cpu-x onlyoffice-desktopeditors brave-browser tor handbrake kdenlive git
+sudo apt install -y cpu-x google-chrome-stable onlyoffice-desktopeditors brave-browser tor handbrake kdenlive git
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/josetg01/oh-my-bash/master/tools/install.sh)"
