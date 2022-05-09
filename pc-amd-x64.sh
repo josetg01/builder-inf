583d0256dbb764e58a95fd0cb1781f48ff8c7911
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-09 19:08:22 +0200

    Update pc-amd-x64.sh

--- pc-amd-x64.sh
+++ pc-amd-x64.sh
@@ -13,5 +13,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
 sudo apt install -y cpu-x google-chrome-stable onlyoffice-desktopeditors brave-browser tor handbrake kdenlive git gimp
+mkdir -p ~/.config/GIMP/2.10/
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/josetg01/oh-my-bash/master/tools/install.sh)"
