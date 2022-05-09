1f9869489fa99d8644ac3b52b679df7801209f27
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-09 16:51:59 +0200

    Update pc-amd-x64.sh

--- pc-amd-x64.sh
+++ pc-amd-x64.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 cd /tmp/
-wget 
+sudo apt install apt-transport-https curl
 
 #Instalar Google Chrome desde repositorios oficiales
 sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
@@ -16,6 +16,7 @@ sudo flatpak install flathub org.gimp.GIMP -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo add-apt-repository ppa:kdenlive/kdenlive-stable
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
-sudo apt install cpu-x ffmpeg obs-studio onlyoffice-desktopeditors kdenlive
+sudo apt install cpu-x ffmpeg obs-studio onlyoffice-desktopeditors kdenlive brave-browser
 sudo flatpak install flathub fr.handbrake.ghb -y
