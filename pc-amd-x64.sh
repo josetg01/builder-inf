020f720b3c432cb6ab4b47eff8fc1417addc5ca5
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-09 19:15:51 +0200

    Update pc-amd-x64.sh

--- pc-amd-x64.sh
+++ pc-amd-x64.sh
@@ -16,3 +16,10 @@ sudo apt install -y cpu-x google-chrome-stable onlyoffice-desktopeditors brave-b
 mkdir -p ~/.config/GIMP/2.10/
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/josetg01/oh-my-bash/master/tools/install.sh)"
+
+#Instalacion kernel mod xanmod
+echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list
+wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key --keyring /etc/apt/trusted.gpg.d/xanmod-kernel.gpg add -
+sudo apt update && sudo apt install -y linux-xanmod
+sudo apt install -y amd64-microcode
+echo 'net.core.default_qdisc = fq_pie' | sudo tee /etc/sysctl.d/90-override.conf
