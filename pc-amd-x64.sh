2526d37e7abd26730f25613248e7e784fdfe731c
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-09 18:46:52 +0200

    Update pc-amd-x64.sh

--- pc-amd-x64.sh
+++ pc-amd-x64.sh
@@ -16,5 +16,4 @@ sudo flatpak install flathub org.gimp.GIMP -y
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
-sudo apt install -y cpu-x onlyoffice-desktopeditors brave-browser tor
-sudo flatpak install flathub fr.handbrake.ghb -y
+sudo apt install -y cpu-x onlyoffice-desktopeditors brave-browser tor handbrake kdenlive
