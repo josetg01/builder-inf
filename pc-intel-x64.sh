73511e24faca6e58ecc6f585f150e732e4949eaa
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-21 19:16:08 +0200

    Update pc-intel-x64.sh

--- pc-intel-x64.sh
+++ pc-intel-x64.sh
@@ -8,7 +8,7 @@ sudo wget https://dl.google.com/linux/linux_signing_key.pub
 sudo apt-key add linux_signing_key.pub
 
 #Instalacion de diversos software
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && sudo echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' >> /etc/apt/sources.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
 sudo apt install -y cpu-x google-chrome-stable onlyoffice-desktopeditors brave-browser tor handbrake kdenlive git gimp
