435f71962b0479a2e845e28028cafb0879018376
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-21 20:57:55 +0200

    Update reduce-zorin.sh

--- reduce-zorin.sh
+++ reduce-zorin.sh
@@ -8,6 +8,18 @@ sudo apt purge -y \
   gnome-mahjongg \
   remmina \
   pitivi \
-  aisleriot \
-  gnome-tour
-sudo apt autoremove -y && sudo apt update && sudo apt upgrade -y
+  aisleriot 
+sudo apt autoremove -y
+
+#Añadir repositorios oficiales de Google Chrome
+sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list
+sudo wget https://dl.google.com/linux/linux_signing_key.pub
+sudo apt-key add linux_signing_key.pub
+
+#Instalacion de diversos software
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get update
+
+sudo apt install -y google-chrome-stable brave-browser
+
