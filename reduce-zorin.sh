caa281614df27d6dac192fb41cfaca3ed891c17d
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-21 20:47:12 +0200

    Update reduce-zorin.sh

--- reduce-zorin.sh
+++ reduce-zorin.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo apt purge -y xfburn gnome-sudoku quadrapassel gnome-mines gnome-mahjongg remmina \
-  pitivi
+sudo apt purge -y \
+  xfburn \
+  gnome-sudoku \
+  quadrapassel \
+  gnome-mines \
+  gnome-mahjongg \
+  remmina \
+  pitivi \
+  aisleriot
 sudo apt autoremove -y
