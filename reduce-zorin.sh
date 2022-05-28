93140ac5b268df0cb9d871ae36d001e291fa71b2
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-28 11:01:12 +0200

    Update reduce-zorin.sh

--- reduce-zorin.sh
+++ reduce-zorin.sh
@@ -28,5 +28,5 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
 
-sudo apt install -y google-chrome-stable brave-browser vscode
+sudo apt install -y google-chrome-stable brave-browser code
 
