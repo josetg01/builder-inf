4d267547571a7d39e4e6cb65f6ce6a41f1239b4f
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-05-28 10:51:40 +0200

    Update reduce-zorin.sh

--- reduce-zorin.sh
+++ reduce-zorin.sh
@@ -16,10 +16,17 @@ sudo echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" 
 sudo wget https://dl.google.com/linux/linux_signing_key.pub
 sudo apt-key add linux_signing_key.pub
 
+#Añadir repositorio VSCode
+sudo apt-get install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
 #Instalacion de diversos software
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
 
-sudo apt install -y google-chrome-stable brave-browser
+sudo apt install -y google-chrome-stable brave-browser vscode
 
