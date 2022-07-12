6ba7abc31fa151c36984ec5eb21707b9fa9debfd
josetg01 <35702013+josetg01@users.noreply.github.com>
2022-07-12 11:42:45 +0200

    Update pc-amd-x64.sh

--- pc-amd-x64.sh
+++ pc-amd-x64.sh
@@ -11,7 +11,7 @@ sudo apt-key add linux_signing_key.pub
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5 && echo 'deb https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt-get update
-sudo apt install -y cpu-x google-chrome-stable onlyoffice-desktopeditors brave-browser tor handbrake kdenlive git gimp
+sudo apt install -y cpu-x onlyoffice-desktopeditors brave-browser tor handbrake kdenlive git gimp
 mkdir -p ~/.config/GIMP/2.10/
 
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/josetg01/oh-my-bash/master/tools/install.sh)"
